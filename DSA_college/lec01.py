e2574fac01fc6216b22e79ee42600daa8fe485a6
Akanksha Kumari <youakanksha@gmail.com>
2025-01-28 01:50:59 +0530

    wip: dsa lec 01

---
+++ DSA_college/lec01.py
@@ -0,0 +1,14 @@
+#Function: It is reusable blocks of code that perform specific task.
+#def ---- Keyword to define a function. Then,Function Name, then parameters (optional), function only
+
+def add(a,b):
+    return a+b
+
+print(add(5,10))
+
+#Write a code to compute n^m.
+
+def power(a,c):
+    return a**c
+
+print(power(3,3))
